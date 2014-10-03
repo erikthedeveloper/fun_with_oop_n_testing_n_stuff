708156e61b1389b3e8729c1a3b533c9befdccf26
Erik Aybar <eaybar@practicegenius.com>
2014-10-03 11:46:49 -0600

    Removes old method. Adds docs

--- graphing_calculator/src/GraphCalc/InfixToPostfix.php
+++ graphing_calculator/src/GraphCalc/InfixToPostfix.php
@@ -9,7 +9,9 @@ namespace GraphCalc;
  */
 class InfixToPostfix
 {
-
+    /**
+     * @var array
+     */
     protected $priorities = [
         '+' => 1,
         '-' => 1,
@@ -80,58 +82,12 @@ class InfixToPostfix
         return $postfix_string;
     }
 
+
     /**
-     * @param $infix_string
-     * @return string
+     * @param $operator
+     * @return int
      * @author Erik Aybar
      */
-    public function OLDtoPostfix($infix_string)
-    {
-        $postfix_string = "";
-        $stack = [];
-        $this->priorities = $priorities = [
-            '+' => 1,
-            '-' => 1,
-            '*' => 2,
-            '/' => 2
-        ];
-        $this->priorities;
-        $items = explode(" ", $infix_string);
-
-        foreach ($items as $cur_item) {
-
-            if (preg_match('/[\dx]/', $cur_item)) {
-                // new_item -> string
-                $postfix_string .= "{$cur_item} ";
-                continue;
-            }
-
-            if (empty($stack)) {
-                array_push($stack, $cur_item);
-                continue;
-            }
-
-            //When an operator is read
-            //– Pop until the top of the stack has an element of lower
-            // precedence
-            //– Then push it
-            if( $this->opValue($stack[count($stack) - 1]) > $this->opValue($cur_item) ) {
-                $postfix_string .= array_pop($stack) . " ";
-            }
-            // WTF?!?!
-            // > passes simple test
-            // >= passes complex test
-        }
-
-        // Append remaining operators onto string
-        $postfix_string = rtrim($postfix_string);
-        foreach ($stack as $stack_item_operator) {
-            $postfix_string .= " " . $stack_item_operator;
-        }
-
-        return $postfix_string;
-    }
-
     public function opValue($operator)
     {
         if (array_key_exists($operator, $this->priorities)){
