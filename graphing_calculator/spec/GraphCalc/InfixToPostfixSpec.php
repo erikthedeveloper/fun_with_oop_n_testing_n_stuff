827672dd864541f88d4159626947d0e4f0e66e12
Erik Aybar <eaybar@practicegenius.com>
2014-10-03 11:44:51 -0600

    Updated specs... were wrong before

--- graphing_calculator/spec/GraphCalc/InfixToPostfixSpec.php
+++ graphing_calculator/spec/GraphCalc/InfixToPostfixSpec.php
@@ -10,25 +10,29 @@ class InfixToPostfixSpec extends ObjectBehavior
 
     function it_converts_simple_addition_to_postfix()
     {
-        $this->toPostfix('x + 2 + 9')->shouldReturn('x 2 9 +');
+        $this->toPostfix('x + 2 + 9')->shouldReturn('x 2 + 9 +');
     }
 
     function it_converts_addition_and_multiplication_to_postfix()
     {
-        $this->toPostfix('x + 2 + 9 * 3')->shouldReturn('x 2 + 9 3 *');
+        $this->toPostfix('x + 2 + 9 * 3')->shouldReturn('x 2 + 9 3 * +');
+    }
+
+    function it_does_some_other_thing()
+    {
         $this->toPostfix('3 + 4 * 5')->shouldReturn('3 4 5 * +');
     }
-
-    function it_processes_input_with_parentheses()
-    {
-        $this->toPostfix('2 * ( 3 + ( 4 - 5) )')->shouldReturn('2 x - x 4 x - * +');
-    }
-
-    function it_can_also_process_exponents()
-    {
-        $this->toPostfix('( 7 - 8 / x ^ 2 + 5 * x - 4 ) * ( x - 6 )')
-            ->shouldReturn('7 8 x 2 ^ / - 5 x * + 4 - x 6 - *');
-    }
+    //
+    //function it_processes_input_with_parentheses()
+    //{
+    //    $this->toPostfix('2 * ( 3 + ( 4 - 5) )')->shouldReturn('2 x - x 4 x - * +');
+    //}
+    //
+    //function it_can_also_process_exponents()
+    //{
+    //    $this->toPostfix('( 7 - 8 / x ^ 2 + 5 * x - 4 ) * ( x - 6 )')
+    //        ->shouldReturn('7 8 x 2 ^ / - 5 x * + 4 - x 6 - *');
+    //}
 
 
 }
